f8effba433be3755369ceacca69157c641c30df5
iramanaditya-svg <iramanaditya@gmail.com>
2026-08-11 16:33:25 +0530

    refactor: remove database connection handling from index.js

--- server/src/index.js
+++ server/src/index.js
@@ -1,20 +1,4 @@
 import "dotenv/config";
-
-import connectDB from "./db/index.js";
 import app from "./app.js";
 
-let dbConnected = false;
-
-const ensureDBConnection = async () => {
-    if (!dbConnected) {
-        await connectDB();
-        dbConnected = true;
-    }
-};
-
-const handler = async (req, res) => {
-    await ensureDBConnection();
-    return app(req, res);
-};
-
-export default handler;
+export default app;
